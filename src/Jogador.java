f8256e7c1d129a71ae4f382137c9ae96752c1ff0
alicefingiro <alicefingiro@localhost>
2026-04-16 15:02:42 -0300

    criando as classes

--- src/Jogador.java
+++ src/Jogador.java
@@ -7,6 +7,31 @@
  *
  * @author alicefingiro
  */
+
+
 public class Jogador {
-    
+
+    private int idJogador;
+    private String nome;
+    private String sobrenome;
+    private String apelido;
+    private String cpf;
+    private String nacionalidade;
+    private Date dataNascimento;
+
+
+    public Jogador() {
+    }
+
+
+    public Jogador(int idJogador, String nome, String sobrenome, String apelido,
+                   String cpf, String nacionalidade, Date dataNascimento) {
+        this.idJogador = idJogador;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.apelido = apelido;
+        this.cpf = cpf;
+        this.nacionalidade = nacionalidade;
+        this.dataNascimento = dataNascimento;
+    }
 }
--- src/Jogo.java
+++ src/Jogo.java
@@ -8,5 +8,21 @@
  * @author alicefingiro
  */
 public class Jogo {
+
+    private int idJogo;
+    private String descritivo;
+    private double apostaMax;
+    private double premioMax;
+
     
+    public Jogo() {
+    }
+
+    
+    public Jogo(int idJogo, String descritivo, double apostaMax, double premioMax) {
+        this.idJogo = idJogo;
+        this.descritivo = descritivo;
+        this.apostaMax = apostaMax;
+        this.premioMax = premioMax;
+    }
 }
