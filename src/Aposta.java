7cc68d5d02d69515808e307ab879bb56e43c1153
alicefingiro <alicefingiro@localhost>
2026-04-24 19:53:26 -0300

    criacao das apostas

--- src/Aposta.java
+++ src/Aposta.java
@@ -1,15 +1,3 @@
-
-import java.util.ArrayList;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author alicefingiro
- */
 import java.util.Random;
 
 public class Aposta {
@@ -17,30 +5,23 @@ public class Aposta {
     private int idAposta;
     private double valorAposta;
 
-    private Jogador jogador = new Jogador();
-
-    // Tipo da aposta
     private int tipo;
 
-    private int qntdGols;       // tipo 1
-    private Time timeEscolhido; // tipo 2
-    private int tempoEscolhido;   // tipo 3 
-    private String jogadorTime;  // tipo 4
-
+    private int qntdGols;
+    private Time timeEscolhido;
+    private String jogadorEscolhido;
 
     private int golsPartida;
     private Time vencedor;
-    private int tempoResultado;
+    private String jogadorGols;
 
-    public Aposta() {
-    }
+    public Aposta() {}
 
     public Aposta(int idAposta, double valorAposta, int tipo) {
         this.idAposta = idAposta;
         this.valorAposta = valorAposta;
         this.tipo = tipo;
     }
-
 
     public void apostarGols(int gols) {
         this.tipo = 1;
@@ -52,43 +33,25 @@ public class Aposta {
         this.timeEscolhido = sistema.buscarTimePorNome(time);
     }
 
-    public void apostarTempo(int tempo) {
+    public void apostarJogador(String jogador) {
         this.tipo = 3;
-        this.tempoEscolhido = tempo;
+        this.jogadorEscolhido = jogador;
     }
-    
-    public void apostarJogador(String jogadorEscolhido) {
-        this.tipo = 4;
-        this.jogadorTime = jogadorEscolhido;
-    }
-
-   
 
     public void realizarAposta(GerenciadorSistema sistema) {
-        System.out.println("Aposta #" + idAposta + " realizada no valor de R$ " + valorAposta);
-        String mensagem = "Selecione o time que deseja apostar:\n";
-        for(int i = 0; i < sistema.getPartidas().size(); i++)
-        {
-            mensagem += (i + 1) + " " + sistema.getPartidas().get(i).getTimeCasa() + "X" + sistema.getPartidas().get(i).getTimeCasa() + "\n";
+
+        String mensagem = "Selecione o time:\n";
+
+        for (int i = 0; i < sistema.getPartidas().size(); i++) {
+            mensagem += (i + 1) + " - " +
+                sistema.getPartidas().get(i).getTimeCasa().getNome() +
+                " X " +
+                sistema.getPartidas().get(i).getTimeFora().getNome() + "\n";
         }
-        
-       
-        apostarVencedor(sistema, InOut.leString(mensagem));
-        
+
+        String time = InOut.leString(mensagem);
+        apostarVencedor(sistema, time);
     }
-
-  
-
-    //public void gerarResultado() {
-        //Random rand = new Random();
-
-        //golsPartida = rand.nextInt(8); 
-
-        //String[] times = {"Time A", "Time B"};
-        //vencedor = times[rand.nextInt(2)];
-
-        //tempoResultado = rand.nextInt(3); 
-    //}
 
     public void verificarResultado() {
 
@@ -96,44 +59,39 @@ public class Aposta {
 
         switch (tipo) {
 
-            case 1:
+            case 1 -> {
                 System.out.println("Gols na partida: " + golsPartida);
 
                 if (golsPartida > qntdGols) {
                     System.out.println("Acertou!");
-                    System.out.println("Ganhou: R$ " + (valorAposta * 2));
                 } else if (golsPartida == qntdGols) {
                     System.out.println("Quase!");
-                    System.out.println("Ganhou: R$ " + (valorAposta * 1.2));
                 } else {
                     System.out.println("Errou!");
                 }
-                break;
+            }
 
-            case 2: 
+            case 2 -> {
                 System.out.println("Vencedor: " + vencedor);
 
-                if (vencedor.equals(timeEscolhido)) {
+                if (vencedor != null && vencedor.equals(timeEscolhido)) {
                     System.out.println("Acertou!");
-                    System.out.println("Ganhou: R$ " + (valorAposta * 2.5));
                 } else {
                     System.out.println("Errou!");
                 }
-                break;
+            }
 
-            case 3:
-                System.out.println("Tempo do gol: " + tempoResultado);
+            case 3 -> {
+                System.out.println("Jogador: " + jogadorGols);
 
-                if (tempoResultado == tempoEscolhido) {
+                if (jogadorGols != null && jogadorGols.equals(jogadorEscolhido)) {
                     System.out.println("Acertou!");
-                    System.out.println("Ganhou: R$ " + (valorAposta * 3));
                 } else {
                     System.out.println("Errou!");
                 }
-                break;
+            }
 
-            default:
-                System.out.println("Tipo de aposta inválido!");
+            default -> System.out.println("Tipo inválido!");
         }
     }
 
@@ -144,8 +102,8 @@ public class Aposta {
     public double getValor() {
         return valorAposta;
     }
-    
-    public void definirValor(double valor){
+
+    public void definirValor(double valor) {
         this.valorAposta = valor;
     }
 
@@ -161,17 +119,7 @@ public class Aposta {
         return timeEscolhido;
     }
 
-    public int getTempoEscolhido() {
-        return tempoEscolhido;
+    public String getJogadorEscolhido() {
+        return jogadorEscolhido;
     }
-
-    public String getJogadorTime() {
-        return jogadorTime;
-    }
-
-    
-    
-    
-    
-    
 }
